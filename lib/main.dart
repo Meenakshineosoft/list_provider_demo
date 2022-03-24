ba3a69ffd600ff10ef2d89d65f4d8f017441a33c
Meenakshi Bariyekar <yash.agraawal@gmail.com>
2022-03-24 16:56:21 +0530

    Third commit by meenakshi

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:list_provider/user_list_screen.dart';
 import 'package:list_provider/view_model/user_list_view_model.dart';
 import 'package:provider/provider.dart';
+
 void main() => runApp(App());
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
